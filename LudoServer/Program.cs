a057cc835998ac156fa6ac8d7b49344e2250d9bc
Marcelo Rios <chelo38@gmail.com>
2021-06-29 19:02:21 -0400

    Ludo server

---
+++ LudoServer/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using GameServer;
+
+namespace LudoServer
+{
+    class MainClass
+    {
+        private static GServer GServer;
+
+        public static void Main(String[] args)
+        {
+            int ListeningPort;
+            if (args.Length > 0 && ProgramHelperFunctions.GetPort(args[0], out ListeningPort))
+            {
+                GServer = new GServer(ListeningPort);
+                GServer.Start();
+            }
+            else
+            {
+                Console.WriteLine("Server port expected as first and only argument (Invalid port or no port supplied).");
+            }
+        }
+    }
+}
---
+++ LudoServer/TcpServer/TcpProtocol/Protocol.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace TcpProtocol
+{
+    public abstract class Protocol
+    {
+        public static Package GetPackage(string type, Object obj = null)
+        {
+            byte code = TypeCode(type);
+            switch (type)
+            {
+                case "keep":        return new Package(code);
+                case "message":     return new Package(code, obj);
+                default:            return null;
+            }
+        }
+
+        public static String TypeName(int actionCode)
+        {
+            switch (actionCode)
+            {
+                case 0:     return "keep";
+                case 1:     return "message";
+                default:    return null;
+            }
+        }
+
+        public static byte TypeCode(string actionName)
+        {
+            switch (actionName)
+            {
+                case "keep":    return 0;
+                case "message": return 1;
+                default:        return 255; // null
+            }
+        }
+    }
+}
